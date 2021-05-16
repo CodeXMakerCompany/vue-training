f690923ebe95db0d1333ed5a64c33e075eea5417
CodeXMakerCompany <codexmaker12@gmail.com>
2021-05-16 18:34:32 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Main msg="Well well well, see who's here." />
+    <p class="container">
+      <b-alert variant="success" show
+        >From main component : {{ courseGoal }}</b-alert
+      >
+    </p>
+    <p class="container">
+      <b-alert variant="success" show>
+        <a v-bind:href="link">Codexmaker Page</a></b-alert
+      >
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from "./components/main.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
+    Main
+  },
+  data() {
+    return {
+      courseGoal: "Learn vue", //String
+      link: "https://codexmaker.xyz/"
+    };
   }
-}
+};
 </script>
 
 <style>
